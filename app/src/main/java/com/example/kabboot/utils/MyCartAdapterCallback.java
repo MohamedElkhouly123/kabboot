f4932b13ea63f10d9c7c9762ec6ca8f820652cd0
MohamedElkhouly123 <mohamedelkholy959@gmail.com>
2021-02-22 20:07:49 +0200

    new

--- app/src/main/java/com/example/kabboot/utils/MyCartAdapterCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.kabboot.utils;
-
-
-public interface MyCartAdapterCallback {
-
-       void onMethodCallback(int allProductsTotalPrice);
-}
--- app/src/main/java/com/example/kabboot/utils/SearchDialogCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.kabboot.utils;
-
-
-public interface SearchDialogCallback {
-
-
-       void filterOnMethodCallback(String searchName, String searchprice, String vendorIdValue, String productCatIdValue);
-}
